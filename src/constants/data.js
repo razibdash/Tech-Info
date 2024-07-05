75a7ac70d9d83f0735535de4f3fa1a59617620a0
avronilrajib <avronilrajib@gmail.com>
2024-07-05 16:04:42 +0600

    add data or logo

---
+++ src/constants/data.js
@@ -0,0 +1,148 @@
+const what_we_do=[
+   {
+     image:`this`,
+     title:"web design",
+     paragraph:"this is text",
+   },
+   {
+     image:`this`,
+     title:"web design",
+     paragraph:"this is text",
+   },
+   {
+     image:`this`,
+     title:"web design",
+     paragraph:"this is text",
+   },
+   {
+     image:`this`,
+     title:"web design",
+     paragraph:"this is text",
+   },
+   {
+     image:`this`,
+     title:"web design",
+     paragraph:"this is text",
+   },
+   {
+     image:`this`,
+     title:"web design",
+     paragraph:"this is text",
+   },
+];
+
+const features=[
+    {
+        icon:`image link`,
+        title:"this is title",
+        text:"dummy text",
+    },
+    {
+        icon:`image link`,
+        title:"this is title",
+        text:"dummy text",
+    },
+    {
+        icon:`image link`,
+        title:"this is title",
+        text:"dummy text",
+    },
+    {
+        icon:`image link`,
+        title:"this is title",
+        text:"dummy text",
+    },
+    {
+        icon:`image link`,
+        title:"this is title",
+        text:"dummy text",
+    },
+]
+const pakages=[
+    {
+        type:"Baisc",
+        service_list:[
+            '5 websites',
+            "15 e-mails",
+            '5 mysql database',
+            '10 GB storage'
+        ],
+        price:"5",
+    },
+    {
+        type:"Baisc",
+        service_list:[
+            '5 websites',
+            "15 e-mails",
+            '5 mysql database',
+            '10 GB storage'
+        ],
+        price:"5",
+    },
+    {
+        type:"Baisc",
+        service_list:[
+            '5 websites',
+            "15 e-mails",
+            '5 mysql database',
+            '10 GB storage'
+        ],
+        price:"5",
+    },
+];
+
+//PROJECT LIST DATA
+const project=[
+    {
+        image:`this is image`,
+        title:"technical aid"
+    },
+    {
+        image:`this is image`,
+        title:"technical aid"
+    },
+    {
+        image:`this is image`,
+        title:"technical aid"
+    },
+    {
+        image:`this is image`,
+        title:"technical aid"
+    },
+    {
+        image:`this is image`,
+        title:"technical aid"
+    },
+    {
+        image:`this is image`,
+        title:"technical aid"
+    },
+];
+
+// ITEMS 
+const items=[
+    {
+        img:`this link`,
+        name:"ashley cole",
+        post:"management",
+    },
+    {
+        img:`this link`,
+        name:"ashley cole",
+        post:"management",
+    },
+    {
+        img:`this link`,
+        name:"ashley cole",
+        post:"management",
+    },
+    {
+        img:`this link`,
+        name:"ashley cole",
+        post:"management",
+    },
+];
+
+export default {
+    what_we_do,pakages,project,items,features
+}
